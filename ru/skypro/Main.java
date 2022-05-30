cb1fe0d704cb2799b10281e0a9df64dda2cd41d6
user <anatoliy6791>
2022-05-30 15:25:33 +0300

    3

--- ru/skypro/Main.java
+++ ru/skypro/Main.java
@@ -1,8 +1,34 @@
 package ru.skypro;
 
-public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public class Main {
+        public static void main(String[] args) {
+            Car[] cars = {
+                    new Car("car1", 4),
+                    new Car("car2", 3)
+            };
+
+
+            Truck[] trucks = {
+                    new Truck("Truck1", 7),
+                    new Truck("Truck2", 8),
+            };
+
+
+            Bicycle[] bicycles = {
+                    new Bicycle("bicycle1", 2),
+                    new Bicycle("bicycle2", 2),
+            };
+
+
+            ServiceStationFor serviceStationFor = new ServiceStationFor();
+            printReport(serviceStationFor, cars, trucks, bicycles);
+        }
+
+        private static void printReport(ServiceStation serviceStation, Car[] cars, Truck[] trucks, Bicycle[] bicycles) {
+            serviceStation.checkCar(cars);
+            serviceStation.checkTruck(trucks);
+            serviceStation.checkBicycle(bicycles);
+        }
     }
-}
+
--- ru/skypro/Truck.java
+++ ru/skypro/Truck.java
@@ -3,15 +3,9 @@ package ru.skypro;
 public class Truck extends Car {
 
     public Truck(String modelName, int wheelsCount) {
-        super(modelName,wheelsCount);    }
-
-    public void updateTyre() {
-        System.out.println("Меняем покрышку");
+        super(modelName,wheelsCount);
     }
 
-    public void checkEngine() {
-        System.out.println("Проверяем двигатель");
-    }
 
     public void checkTrailer() {
         System.out.println("Проверяем прицеп");
